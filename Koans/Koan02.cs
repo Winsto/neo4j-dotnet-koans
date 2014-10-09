2adb0e9521810d13829935986c21162947ea2c82
winsto <winsto@live.co.uk>
2014-10-09 02:27:59 +0100

    The next method. Quite a bit of fannying around with test initialise and cleanup to get this working!

--- Koans/Koan02.cs
+++ Koans/Koan02.cs
@@ -11,6 +11,16 @@ namespace Koans
     {
         private GraphClient graphClient;
 
+        [ClassInitialize]
+        public static void MakeSureDatabaseIsEmptyBeforeWeStart(TestContext context)
+        {
+            var graphClient = new GraphClient(new Uri("http://localhost:7474/db/data"));
+
+            graphClient.Connect();
+
+            DeleteAllNodesAndRelationshipsFromGraphClient(graphClient);
+        }
+
         [TestInitialize]
         public void CreateANewClient()
         {
@@ -34,10 +44,31 @@ namespace Koans
             Assert.AreEqual(1, getAllNodes.Results.Count());
         }
 
+        [TestMethod]
+        public void ShouldCreateASingleNodeWithSomeProperties()
+    {
+        // YOUR CODE GOES HERE
+        // SNIPPET_START
+
+        graphClient.Cypher.Create("(n { firstname : 'Tom', lastname : 'Baker' })").ExecuteWithoutResults();
+
+        // SNIPPET_END
+
+        var matchedNodes = graphClient.Cypher.Match("(n {firstname: 'Tom', lastname: 'Baker'})").Return<object>("n");
+
+        /* Geek question: if you've read ahead, what's the Big O cost of this match? */
+
+        Assert.AreEqual(1, matchedNodes.Results.Count());
+    }
         [TestCleanup]
-        private void DeleteAllNodesAndRelationships()
+        public void DeleteAllNodesAndRelationships()
         {
-            graphClient.Cypher.Start(new { allNodes = All.Nodes }).OptionalMatch("()-[allEdges]-()").Delete("allNodes, allEdges").ExecuteWithoutResults();
+            DeleteAllNodesAndRelationshipsFromGraphClient(graphClient);
+        }
+
+        private static void DeleteAllNodesAndRelationshipsFromGraphClient(GraphClient deletionTarget)
+        {
+            deletionTarget.Cypher.Start(new { allNodes = All.Nodes }).OptionalMatch("()-[allEdges]-()").Delete("allNodes, allEdges").ExecuteWithoutResults();
         }
     }
 }
